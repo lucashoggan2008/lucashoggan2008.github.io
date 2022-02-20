11b15ebda1e42c321e128a113fc117210b898b8f
lucashoggan2008 <77905024+lucashoggan2008@users.noreply.github.com>
2022-02-20 12:43:02 +0000

    commit

--- src/components/DragDiv.jsx
+++ src/components/DragDiv.jsx
@@ -2,7 +2,7 @@ import {useSpring, animated} from '@react-spring/web'
 import {useDrag} from '@use-gesture/react'
 
 const DragDiv = (props) => {
-    const [{x, y}, api] = useSpring(() => ({x:0}))
+    const [{x, y}, api] = useSpring(() => ({x:0, y:0}))
     const bind = useDrag(({down, movement: [mx, my]}) => api.start({x:down ? mx:0, y:down ? my:0}), {axis: 'lock'})
     return <animated.div className={props.className} {...bind()} style={{x, y}}>{props.children}</animated.div>
 }
